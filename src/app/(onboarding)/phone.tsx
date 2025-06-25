1ab0c87e47a688f6e0536b22847130af913f1c50
Thiago Munich <thiago.munich@direcional.com.br>
2025-06-24 21:45:00 -0300

    feat: update phone onboarding screen with new titles, subtitles, and input field for phone number

--- src/app/(onboarding)/phone.tsx
+++ src/app/(onboarding)/phone.tsx
@@ -1,8 +1,11 @@
 import React from "react"
 
-import { Text, View } from "react-native"
+import { View } from "react-native"
 
 import { Container } from "@/components/onboarding/container"
+import { SingleLineInput } from "@/components/onboarding/inputs/single-line-input"
+import { Subtitle } from "@/components/onboarding/subtitle"
+import { Title } from "@/components/onboarding/title"
 import { ThemedButton } from "@/components/shared/button"
 import { router } from "expo-router"
 
@@ -13,10 +16,16 @@ export default function Phone() {
 
   return (
     <Container>
-      <Text>phone</Text>
+      <Title>Let's stay connected!</Title>
+
+      <Subtitle>How about important updates and notifications?</Subtitle>
+
+      <SingleLineInput placeholder="Type your phone here..." keyboardType="number-pad" />
+
       <View className="absolute bottom-0 w-full">
         <ThemedButton onPress={handleGoToRoles}>
           <ThemedButton.Text>CONTINUE</ThemedButton.Text>
+          <ThemedButton.Badge>3 of 6</ThemedButton.Badge>
         </ThemedButton>
       </View>
     </Container>
