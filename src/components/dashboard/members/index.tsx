7cfe1633a8101cb4f8d8dc23e1c7650f639d2acb
Thiago Munich <thiago.munich@direcional.com.br>
2025-06-25 17:53:00 -0300

    feat: add confirmation alert for member deletion in dashboard

--- src/components/dashboard/members/index.tsx
+++ src/components/dashboard/members/index.tsx
@@ -2,7 +2,7 @@ import React, { memo } from "react"
 
 import { deleteMemberById } from "@/services/members"
 import { AddMemberForm } from "@/store/add-member"
-import { FlatList } from "react-native"
+import { Alert, FlatList } from "react-native"
 import { Member } from "../member"
 
 interface Props {
@@ -10,12 +10,33 @@ interface Props {
 }
 
 export const Members = memo(({ data }: Props) => {
+  const confirmDeleteMember = (member: AddMemberForm) => {
+    Alert.alert(
+      "REMOVING ONGOING \n",
+      `Do you really want to remove ${member.name} from the list?`,
+      [
+        {
+          text: "Cancel",
+          style: "cancel",
+        },
+        {
+          text: "Delete",
+          style: "destructive",
+          onPress: async () => {
+            await deleteMemberById(member.id)
+          },
+        },
+      ],
+      { cancelable: true }
+    )
+  }
+
   return (
     <FlatList
       data={data}
       keyExtractor={(item) => item.id}
       showsVerticalScrollIndicator={false}
-      renderItem={({ item }) => <Member data={item} onPress={async () => await deleteMemberById(item.id)} />}
+      renderItem={({ item }) => <Member data={item} onLongPress={() => confirmDeleteMember(item)} />}
       contentContainerStyle={{ gap: 16, paddingTop: 12, paddingBottom: 12 }}
     />
   )
